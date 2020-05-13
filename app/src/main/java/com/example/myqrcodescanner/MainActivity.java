fe611bcef6e223247d11fa48cfafb663fc5d5057
Derrek <dschuurm@ucsd.edu>
2020-05-13 14:03:59 -0700

    status is working

--- app/src/main/java/com/example/myqrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/myqrcodescanner/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -16,8 +19,15 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    String[] QR_CODES = {"blue fish", "Shark", "Yellow Fish", "Status_4"};
+    private final int[] BUTTON_ID = {
+            (R.id.btn_status_1),
+            (R.id.btn_status_2),
+            (R.id.btn_status_3),
+            (R.id.btn_status_4)
+    };
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.view_result);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+
+        for(int i = 0; i < QR_CODES.length; i++)  {
+            Button button = (Button)findViewById(BUTTON_ID[i]);
+            button.setText(QR_CODES[i]);
+            button.setBackgroundColor(Color.RED);
+        }
+
+
 
     }
 
@@ -42,9 +60,24 @@ public class MainActivity extends AppCompatActivity {
             if( intentResult.getContents() == null)  {
                 textView.setText("Cancelled");
             }  else  {
+                checkForStatus(intentResult);
                 textView.setText(intentResult.getContents());
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    void checkForStatus(IntentResult intentResult)  {
+        String results = intentResult.getContents();
+        for(int i = 0; i < QR_CODES.length; i++)  {
+            Log.d("myTag", results);
+            if( QR_CODES[i].equals(results) )  {
+                Button button = (Button)findViewById(BUTTON_ID[i]);
+                button.setBackgroundColor(Color.GREEN);
+
+            }
+        }
+    }
+
+
 }
